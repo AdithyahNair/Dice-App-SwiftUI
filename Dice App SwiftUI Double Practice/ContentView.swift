395d080edf6b44fc2a503d2dacae9056c4e7578d
Adithyah Nair <adithyah.nair@gmail.com>
2022-04-28 07:18:44 +0530

    Initial Commit

---
+++ Dice App SwiftUI Double Practice/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dice App SwiftUI Double Practice
+//
+//  Created by Adithyah Nair on 28/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dice App SwiftUI Double Practice/Dice_App_SwiftUI_Double_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dice_App_SwiftUI_Double_PracticeApp.swift
+//  Dice App SwiftUI Double Practice
+//
+//  Created by Adithyah Nair on 28/04/22.
+//
+
+import SwiftUI
+
+@main
+struct Dice_App_SwiftUI_Double_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
